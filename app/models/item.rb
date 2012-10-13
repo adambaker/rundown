a0d76f003453c9cee59a57a30c2e8339ea9774ca
Adam C. Baker <adam.durandal@gmail.com>
2012-10-13 15:13:12 -0700

    add validations and price handling to model.

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,33 @@ class Item
   field :name, type: String
   field :price, type: Integer
   field :quantity, type: Integer
+
+  validates :name, presence: true
+  validates :quantity, numericality: {
+    only_integer: true, greater_than_or_equal_to: 0
+  }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  def price=(price_str)
+    self.write_attribute(:price, price_to_int(price_str))
+  end
+
+  def price
+    price_int = self.read_attribute(:price) or return ''
+    price_str = price_int.to_s
+    if price_int < 10
+      price_str.insert(0, '0.0')
+    else
+      price_str.insert(-3, '.')
+    end
+  end
+
+  private
+    def price_to_int(price_str)
+      begin
+        price_int = (Float(price_str.strip.gsub(/\$|\,/,''))*100).round
+      rescue Exception => e
+        price_int = ''
+      end
+    end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
 describe Item do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @attr = {
+      name: 'I heart minimalism',
+      price: '$20.50',
+      quantity: 4,
+    }
+  end
+
+  it 'should accept a valid item' do
+    Item.new(@attr).should be_valid
+  end
+
+  it 'should reject an item with no name' do
+    Item.new(@attr.merge({name: ''})).should_not be_valid
+  end
+
+  it 'should require a non-negative quantity' do
+    Item.new(@attr.merge({quantity: ''})).should_not be_valid
+    Item.new(@attr.merge({quantity: '-3'})).should_not be_valid
+  end
+
+  it 'should handle decimals and commas' do
+    item = Item.create(@attr.merge({price: '1,020.50'}))
+    item.price.should == '1020.50'
+  end
+
+  it 'should require non-negative price' do
+    Item.new(@attr.merge({price: ''})).should_not be_valid
+    Item.new(@attr.merge({price: '-3'})).should_not be_valid
+  end
 end
