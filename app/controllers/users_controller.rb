efe2f4746ed648a0ee8cc6125c0028c75943c32b
Adam C. Baker <adam.durandal@gmail.com>
2012-11-05 02:39:19 -0800

    user create action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
   def create
+    respond_to do |format|
+      if User.create params[:user]
+        format.html { redirect_to '/', notice: 'New user account created' }
+      end
+    end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
 describe UsersController do
+  before :all do
+    User::Email.create email: 'fake@phony.lie'
+  end
 
+  before :each do
+    User.delete_all
+  end
+
+  describe 'POST create' do
+    describe 'with valid attributes' do
+      it 'should create a new user' do
+        expect do
+          post :create, user: user_attr
+        end.to change(User, :count).by(1)
+      end
+
+      it 'should redirect to root' do
+        post :create, user: user_attr
+        response.should redirect_to('/')
+      end
+
+      it 'should post a notice' do
+        post :create, user: user_attr
+        flash[:notice].should_not be_nil
+      end
+    end
+  end
 end
