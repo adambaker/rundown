a74ec4a1e3e12ff5d377ebd8b9d6dbdcce35432f
Adam C. Baker <adam.durandal@gmail.com>
2012-11-05 02:34:57 -0800

    Extract useful user attrs into test helper

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,31 +27,25 @@ describe User do
 
   before :each do
     User.delete_all
-    @attr = {
-      provider: 'goo',
-      uid: '123450',
-      name: 'somebody',
-      email: 'fake@phony.lie',
-    }
   end
 
   it "should accept a valid user" do
-    User.create! @attr
+    User.create! user_attr
   end
 
   it "should reject a user whose email isn't whitelisted" do
-    User.new(@attr.merge email: 'another@email.com').should_not be_valid
+    User.new(user_attr.merge email: 'another@email.com').should_not be_valid
   end
 
   it 'should reject blank attributes' do
     [:provider, :uid, :name].each do |k|
-      User.new(@attr.merge k => '').should_not be_valid
+      User.new(user_attr.merge k => '').should_not be_valid
     end
   end
 
   it 'should require unique uid within a provider' do
-    User.create @attr
-    User.new(@attr).should_not be_valid
+    User.create user_attr
+    User.new(user_attr).should_not be_valid
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,10 +9,6 @@ require 'rspec/autorun'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
@@ -20,11 +16,19 @@ RSpec.configure do |config|
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
+  config.infer_base_class_for_anonymous_controllers = true
 
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  #config.order = "random"
+  def user_attr
+    {
+      provider: 'goo',
+      uid: '123450',
+      name: 'somebody',
+      email: 'fake@phony.lie',
+    }
+  end
+
+  def a_user
+    User::Email.create email: 'fake@phony.lie'
+    User.create user_attr
+  end
 end
