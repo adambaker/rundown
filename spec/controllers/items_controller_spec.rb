b690e32cbd7e12f858d0edbfc6389b96a3670fb2
Adam C. Baker <adam.durandal@gmail.com>
2012-11-10 14:38:37 -0600

    Clean up item controller specs.

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -23,7 +23,7 @@ describe ItemsController do
   describe "GET show" do
     it "assigns the requested item as @item" do
       item = Item.create! valid_attributes
-      get :show, {:id => item.to_param}, valid_session
+      get :show, {id: item}, valid_session
       assigns(:item).should eq(item)
     end
   end
@@ -39,18 +39,18 @@ describe ItemsController do
     describe "with valid params" do
       it "creates a new Item" do
         expect {
-          post :create, {:item => valid_attributes}, valid_session
+          post :create, {item: valid_attributes}, valid_session
         }.to change(Item, :count).by(1)
       end
 
       it "assigns a newly created item as @item" do
-        post :create, {:item => valid_attributes}, valid_session
+        post :create, {item: valid_attributes}, valid_session
         assigns(:item).should be_a(Item)
         assigns(:item).should be_persisted
       end
 
       it "redirects to the created item" do
-        post :create, {:item => valid_attributes}, valid_session
+        post :create, {item: valid_attributes}, valid_session
         response.should redirect_to(Item.last)
       end
     end
@@ -58,13 +58,13 @@ describe ItemsController do
     describe "with invalid params" do
       it "assigns a newly created but unsaved item as @item" do
         Item.any_instance.stub(:save).and_return(false)
-        post :create, {:item => {}}, valid_session
+        post :create, {item: {}}, valid_session
         assigns(:item).should be_a_new(Item)
       end
 
       it "re-renders the 'new' template" do
         Item.any_instance.stub(:save).and_return(false)
-        post :create, {:item => {}}, valid_session
+        post :create, {item: {}}, valid_session
         response.should render_template("new")
       end
     end
@@ -74,19 +74,21 @@ describe ItemsController do
     describe "with valid params" do
       it "updates the requested item" do
         item = Item.create! valid_attributes
-        Item.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, {:id => item.to_param, :item => {'these' => 'params'}}, valid_session
+        Item.any_instance.should_receive(:update_attributes).with(
+          {'a' => 'b'}
+        )
+        put :update, {id: item, item: {'a' => 'b'}}, valid_session
       end
 
       it "assigns the requested item as @item" do
         item = Item.create! valid_attributes
-        put :update, {:id => item.to_param, :item => valid_attributes}, valid_session
+        put :update, {id: item, item: valid_attributes}, valid_session
         assigns(:item).should eq(item)
       end
 
       it "redirects to the item" do
         item = Item.create! valid_attributes
-        put :update, {:id => item.to_param, :item => valid_attributes}, valid_session
+        put :update, {id: item, item: valid_attributes}, valid_session
         response.should redirect_to(item)
       end
     end
@@ -95,14 +97,14 @@ describe ItemsController do
       it "assigns the item as @item" do
         item = Item.create! valid_attributes
         Item.any_instance.stub(:save).and_return(false)
-        put :update, {:id => item.to_param, :item => {}}, valid_session
+        put :update, {id: item, item: {}}, valid_session
         assigns(:item).should eq(item)
       end
 
       it "re-renders the 'show' template" do
         item = Item.create! valid_attributes
         Item.any_instance.stub(:save).and_return(false)
-        put :update, {:id => item.to_param, :item => {}}, valid_session
+        put :update, {id: item, item: {}}, valid_session
         response.should render_template("show")
       end
     end
@@ -112,13 +114,13 @@ describe ItemsController do
     it "destroys the requested item" do
       item = Item.create! valid_attributes
       expect {
-        delete :destroy, {:id => item.to_param}, valid_session
+        delete :destroy, {id: item}, valid_session
       }.to change(Item, :count).by(-1)
     end
 
     it "redirects to the items list" do
       item = Item.create! valid_attributes
-      delete :destroy, {:id => item.to_param}, valid_session
+      delete :destroy, {id: item}, valid_session
       response.should redirect_to(items_url)
     end
   end
