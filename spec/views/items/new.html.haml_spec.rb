7a81c4d10e5aac4633e5019d5a5840ada4c4df0a
Adam C. Baker <adam.durandal@gmail.com>
2012-11-11 17:17:02 -0600

    test new item view for public attr

--- spec/views/items/new.html.haml_spec.rb
+++ spec/views/items/new.html.haml_spec.rb
@@ -12,11 +12,16 @@ describe "items/new" do
   it "renders new item form" do
     render
 
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", :action => items_path, :method => "post" do
-      assert_select "input#item_name", :name => "item[name]"
-      assert_select "input#item_price", :name => "item[price]"
-      assert_select "input#item_quantity", :name => "item[quantity]"
+      assert_select "input#item_name", name: "item[name]"
+      assert_select "input#item_price", name: "item[price]"
+      assert_select "input#item_quantity", name: "item[quantity]"
+      assert_select('input#item_public_attributes_title',
+        name: 'item[public_attributes][title]'
+      )
+      assert_select('textarea#item_public_attributes_body',
+        name: 'item[public_attributes][body]'
+      )
     end
   end
 end
